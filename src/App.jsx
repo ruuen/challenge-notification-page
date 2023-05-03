a5279b944ff4b9cbaad8cd2cfebea2637637451d
ruuen <max@madebyruuen.com>
2023-05-03 15:51:02 +1000

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import NotificationWindow from "./components/NotificationWindow/NotificationWindow";
+
+function App() {
+  return (
+    <main className="page-container">
+      <NotificationWindow />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/NotificationWindow/NotificationWindow.jsx
@@ -0,0 +1,3 @@
+export default function NotificationWindow() {
+  return <div>NotificationWindow</div>;
+}
